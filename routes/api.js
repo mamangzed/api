279154feb646ba6bbe84978747fe425a27dfa29e
mamangzed <45355372+mamangzed@users.noreply.github.com>
2021-04-06 04:24:03 +0700

    Update api.js

--- routes/api.js
+++ routes/api.js
@@ -8,7 +8,7 @@ var zahirr = db.get("zahirr");
 	console.log('')
 }
 
-var creatorList = ['@zahirrr','@zhirrrgans','@zhirr_ajalah','@zahiranjay', '@zahirganssss','@zhirrganteng','@zahirrgantengg'];
+var creatorList = ['@wanda','@wandagans','@wandaganteng','@wandahadis', '@wandaganssss','@wanda','@wandagantengg'];
 var creator = creatorList[Math.floor(Math.random() * creatorList.length)];
 
 
